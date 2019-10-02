ed975a697a26b4d40d1de5e57b93f9a70932b61c
Shubham <Shubham@Lenovo-PC>
2019-10-02 23:42:42 +0530

    2nd commit Completed all the bean classes. Entity relations are specified.
    Road Forward:(Vineeta and Anirban)
    Check all the bean classes with the given problem statement.
    Write test classes to check if all the entries are going in the
    right places.
    
    Highway Forward:(Vishal and Shubham)
    Design all the screens using JSP.

--- university-admission/src/main/java/com/cg/entity/Application.java
+++ university-admission/src/main/java/com/cg/entity/Application.java
@@ -2,10 +2,13 @@ package com.cg.entity;
 
 import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 
 @Entity
@@ -31,6 +34,11 @@ public class Application {
 	@Column(length = 10)
 	private String status;
 	private Date dateOfInterview;
+	
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "offered_program_id")
+	private ProgramsOffered program;
 	
 	public Application() {
 	}
